322a94606e61c52339510290d0004a2cf2ae6b22
Jacesoul <75877363+Jacesoul@users.noreply.github.com>
2022-02-01 17:42:50 +0900

    Minutes Hours Converter SetUp

---
+++ src/theme.ts
@@ -0,0 +1,14 @@
+import { DefaultTheme } from "styled-components";
+
+export const darkTheme: DefaultTheme = {
+  bgColor: "#2f3640",
+  accentColor: "#ffa801",
+  textColor: "white",
+  cardBgColor: "transparent",
+};
+export const lightTheme: DefaultTheme = {
+  bgColor: "whitesmoke",
+  textColor: "black",
+  accentColor: "#9c88ff",
+  cardBgColor: "white",
+};
